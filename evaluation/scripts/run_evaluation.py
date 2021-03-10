4a37ce1863e036d7f40c72359a39059e8cec8afd
jBialas <julian.bialas@gmail.com>
2021-03-10 18:49:48 +0100

    minor changes in filestructure and added some more datasets in config file

--- evaluation/scripts/run_evaluation.py
+++ evaluation/scripts/run_evaluation.py
@@ -21,10 +21,14 @@ if __name__ == "__main__":
         filename = data_metadata.iloc[i, 0]
         url = data_metadata.iloc[i, 1]
         dataset = data_metadata.iloc[i, 2]
+        run = data_metadata.iloc[i, 3]
 
         ###############################
         # get the data
         ###############################
+        if not run:
+            print("skipping {}...".format(filename))
+            continue
         dh = DataHandler(filename=filename, url=url, dest=PATH_TO_TMP_DIR)
         print("downloading {}...".format(filename))
         dh.download()
@@ -38,48 +42,58 @@ if __name__ == "__main__":
         ################################
         cg = CommandGenerator()
         if platform == "linux" or platform == "linux2":
-        ##################### ORB ###########################
-            command = cg.orb(filename=filename,
-                             path_to_orb=PATH_TO_ORB_SLAM,
-                             path_to_data=PATH_TO_TMP_DIR,
-                             dataset=dataset)
-            print("Running ORB slam on {}!".format(filename))
-            process = subprocess.Popen(command, shell=True)
-            process.wait()
-            print(process.returncode)
-            res_orb_dir = os.path.join(PATH_TO_RESULT_DIR, filename, "ORB")
-            res_orb_img_dir = os.path.join(res_orb_dir, "img")
-            res_orb_raw_dir = os.path.join(res_orb_dir, "raw_out")
-            if not os.path.exists(res_orb_dir):
-                os.makedirs(res_orb_dir, exist_ok=True)
-            if not os.path.exists(res_orb_img_dir):
-                os.makedirs(res_orb_img_dir, exist_ok=True)
-            if not os.path.exists(res_orb_raw_dir):
-                os.makedirs(res_orb_raw_dir, exist_ok=True)
-            # try to copy the output in the right place
-                try:
-                    shutil.move("CameraTrajectory.txt".format(filename), os.path.join(res_orb_raw_dir, "estimated_data.txt"))
-                except:
-                    raise ValueError("Something went wrong! Could not copy output file!")
-            preproc = FramePreprocessor(gt_filepath=os.path.join(PATH_TO_TMP_DIR, filename, "mav0",
-                                                                 "state_groundtruth_estimate0", "data.csv"),
-                                        est_filepath=os.path.join(res_orb_raw_dir, "estimated_data.txt"),
-                                        dataset_type=dataset)
-            print("reading in the result dataframes...")
-            preproc.create_est_pos_df()
-            preproc.create_gt_pos_df()
-            print("aligning the timestamps...")
-            preproc.align_timestamps()
-            print("transforming the coordinate system...")
-            preproc.transform_coordinate_system()
-            print("evaluating...")
-            position_eval = Evaluator(gt_df=preproc.get_gt_pos_df(),
-                                      est_df=preproc.get_est_pos_df())
-            position_eval.create_pos_dif_plots(outdir=res_orb_img_dir)
-            print("cleaning up the download directory...")
-            dh.clean_download_dir()
+            try:
+                ##################### ORB ###########################
+                command = cg.orb(filename=filename,
+                                 path_to_orb=PATH_TO_ORB_SLAM,
+                                 path_to_data=PATH_TO_TMP_DIR,
+                                 dataset=dataset)
+                print("Running ORB slam on {}!".format(filename))
+                process = subprocess.Popen(command, shell=True)
+                process.wait()
+                print(process.returncode)
+                res_orb_dir = os.path.join(PATH_TO_RESULT_DIR, filename, "ORB")
+                res_orb_img_dir = os.path.join(res_orb_dir, "img")
+                res_orb_raw_dir = os.path.join(res_orb_dir, "raw_out")
+                if not os.path.exists(res_orb_dir):
+                    os.makedirs(res_orb_dir, exist_ok=True)
+                if not os.path.exists(res_orb_img_dir):
+                    os.makedirs(res_orb_img_dir, exist_ok=True)
+                if not os.path.exists(res_orb_raw_dir):
+                    os.makedirs(res_orb_raw_dir, exist_ok=True)
+                # try to copy the output in the right place
+                    try:
+                        shutil.move("CameraTrajectory.txt".format(filename), os.path.join(res_orb_raw_dir, "estimated_data.txt"))
+                    except:
+                        raise ValueError("Something went wrong! Could not copy output file!")
+                preproc = FramePreprocessor(gt_filepath=os.path.join(PATH_TO_TMP_DIR, filename, "mav0",
+                                                                     "state_groundtruth_estimate0", "data.csv"),
+                                            est_filepath=os.path.join(res_orb_raw_dir, "estimated_data.txt"),
+                                            dataset_type=dataset)
+                print("reading in the result dataframes...")
+                preproc.create_est_pos_df()
+                preproc.create_gt_pos_df()
+                print("aligning the timestamps...")
+                preproc.align_timestamps()
+                print("transforming the coordinate system...")
+                preproc.transform_coordinate_system()
+                print("evaluating...")
+                position_eval = Evaluator(gt_df=preproc.get_gt_pos_df(),
+                                          est_df=preproc.get_est_pos_df())
+                position_eval.create_pos_dif_plots(outdir=res_orb_img_dir)
+                print("cleaning up the download directory...")
+                dh.clean_download_dir()
+
+            except:
+                raise ValueError("Could not run ORB-Slam")
+
+            finally:
+                # clean up the files
+                dh.clean_download_dir()
 
         else:
             print("Slam algorithms can only be run on linux; skipping...")
+
+
 
 
